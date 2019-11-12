559a6574e8392bd244b267fe21d46c64ce30f39f
Burbage Stacey <rstacey06@gmail.com>
2019-11-12 17:58:04 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,7 @@ class String
   end
 
   def count_sentences
-    self.split(/. ? !/) #{|item| block} => integer (count meth)
+    self.split(/\.|\?|\!/) #{|item| block} => integer (count meth)
 
   end
 end
